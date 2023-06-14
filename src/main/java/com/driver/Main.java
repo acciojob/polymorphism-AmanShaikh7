b20a8870773dba01ac78a6f0343332557d9db269
Aman <amanshaikh7102000@gmail.com>
2023-06-14 19:15:27 +0530

    removed print statement

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String args[]){
         Product p = new Product();
-        int twoparam    = p.product(1,2);
-        int threeparam  = p.product(1,2,3);
-        double dtwoparam = p.product(1.0,2.0);
-        System.out.println(twoparam);
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.0,2.0);
+
     }
 }
 class Product{
