2930c151f0e7799bfe5232e0d96ab98542288c02
Aman <amanshaikh7102000@gmail.com>
2023-06-14 19:08:40 +0530

    Implemented function overloading for product function

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String args[]){
+        Product p = new Product();
+        int twoparam    = p.product(1,2);
+        int threeparam  = p.product(1,2,3);
+        double dtwoparam = p.product(1.0,2.0);
+        System.out.println(twoparam);
+    }
+}
+class Product{
+    public int product(int x, int y){
+        return x*y;
+    }
+    public int product(int x, int y,int z){
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
